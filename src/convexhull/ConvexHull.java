e28b7c809cd2e99fcee023511cd5efba993564af
Ibrahim Abdullah <braimahabdulla>
2017-11-02 07:42:42 +0100

    angle at point computed to find pMax

--- src/convexhull/ConvexHull.java
+++ src/convexhull/ConvexHull.java
@@ -12,7 +12,7 @@ import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
+import java.util.HashSet;   
 import java.util.function.Consumer;
 
 /**
@@ -30,6 +30,9 @@ public class ConvexHull {
      * Sort an array of point the order of non-decreasing x-coordinate 
      * Ties are broken by the y values.
      * 
+     * @param points Array of point to be sorted
+     * @return Array of point sorted in the order of their non-decreasing x-coordinate
+     * with ties broken by the y-coordinate
      */
     public Point[] sortPointInIncreasingXCoordinate(Point [] points) {
         Arrays.sort(points);
@@ -37,30 +40,38 @@ public class ConvexHull {
     }
 
     /**
-     *
-     * @param points
-     * @return 
+     * Find the convex hull of a set of two dimensional point in a plane 
+     * @param points Array of the set of point
+     * @return The convexSet of the given set of point
      */
     public HashSet<Point> findConvexHull(Point [] points) {
         
-        //Sort points in array in order of thier non-decreasing x-xoordinate
-        
+        //return the same points as the convex set if the number of point are
+        //less than or equal to 3.
         if(points.length <= 3){
-            boolean addAll = this.convexSet.addAll(Arrays.asList(points));
+            this.convexSet.addAll(Arrays.asList(points));
             return this.convexSet;
         }
+        
+        //Sort the point in order of the non decreasing x-coordinate 
         Point [] allPoints = sortPointInIncreasingXCoordinate(points);
+        
+        //Select the extreme sorted array
         Point p1 = allPoints[0];
         Point pn = allPoints[allPoints.length-1];
+        
         ArrayList<Point> allPointsA = new ArrayList<>(Arrays.asList(allPoints));
         
         //Add the two extreme point to the convexSet
         this.convexSet.add(p1);
         this.convexSet.add(pn);
+        
+        //Remove the extreme point from consideration
         allPointsA.remove(0);
         allPointsA.remove(allPointsA.size()-1);
         
-        //Find point above and below line P1 and Pn
+        //Find the set of poin that lie on the left and right side of the 
+        //the line formed by point p1 and pn
         ArrayList<Point> pointLeftOfLineP1Pn = findPointLeftOfLine(p1,pn,allPointsA);
         ArrayList<Point> pointRightOfLineP1Pn = findPointRightOfLine(p1,pn,allPointsA);
         
@@ -73,6 +84,7 @@ public class ConvexHull {
     }
     /**
      * Find the set of point that lie above the line formed by p1 and pn
+     * It uses the determinant of the point to the line p1pn
      * @param p1 point
      * @param pn point
      * @param points Array of point
@@ -100,14 +112,11 @@ public class ConvexHull {
      */
     public ArrayList<Point> findPointRightOfLine(Point p1, Point pn, ArrayList<Point> points) {
         ArrayList<Point> pointsRightOfLineP1Pn = new ArrayList<>();
-        points.forEach(new Consumer<Point>() {
-            @Override
-            public void accept(Point a) {
-                double determinant;
-                determinant = (p1.x * pn.y)+(a.x * p1.y) + (pn.x * a.y) - (a.x * pn.y)-(pn.x * p1.y)-(p1.x * a.y);
-                if (determinant < 0) {
-                    pointsRightOfLineP1Pn.add(a);
-                }
+        points.forEach((Point a) -> {
+            double determinant;
+            determinant = (p1.x * pn.y)+(a.x * p1.y) + (pn.x * a.y) - (a.x * pn.y)-(pn.x * p1.y)-(p1.x * a.y);
+            if (determinant < 0) {
+                pointsRightOfLineP1Pn.add(a);
             }
         });
         //Point [] pointRightOfLineP1PnArray = getPointArray2(pointsRightOfLineP1Pn);
@@ -115,21 +124,38 @@ public class ConvexHull {
     }
 
     /**
-     * This method find the above Hull a convex hull
-     * @param pointLeftOfLine
-     * @param p1 point
-     * @param pn point
+     * Find the upper hull of the convex set
+     * @param pointLeftOfLine set of point of the left of line p1 and pn
+     * @param p1 point p1
+     * @param pn point pn
      */
     public void findUpperHull(ArrayList<Point> pointLeftOfLine, Point p1, Point pn) {
         if(!pointLeftOfLine.isEmpty()){
-            int index = 0;
-            double MaxDistance = 0.0;
+            int index = -1;
+            double MaxDistance = -1;
             
             for( int i = 0; i < pointLeftOfLine.size(); i++){
                 double pointDistanceFromLine = getDistanceFromLine(pointLeftOfLine.get(i),p1,pn);
                 if(pointDistanceFromLine > MaxDistance){
                     MaxDistance = pointDistanceFromLine;
                     index = i;
+                }
+                /**
+                 * If two point have the same distance from the line, calculate the 
+                 * the angle at each point and select the point with the larger
+                 * angle as point in the convex set.
+                 */
+                else if((pointDistanceFromLine == MaxDistance) && (index != -1)){
+                    Point pointAtIndex = pointLeftOfLine.get(index);
+                    Point pointAtI = pointLeftOfLine.get(i);
+                    
+                    double angleAtpointAtIndexToP1andPn = findAngleOfPointFromLine(pointAtIndex,p1,pn);
+                    double angleAtpointAtIToP1andPn = findAngleOfPointFromLine(pointAtI,p1,pn);
+                    
+                    if(angleAtpointAtIToP1andPn > angleAtpointAtIndexToP1andPn){
+                        MaxDistance = pointDistanceFromLine;
+                        index = i;
+                    }
                 }
             }
             Point pMax = pointLeftOfLine.get(index);
@@ -151,9 +177,9 @@ public class ConvexHull {
 
     /**
      * 
-     * @param pointRightOfLine
-     * @param p1 point
-     * @param pn point
+     * @param pointRightOfLine Set of point in the right side of the line p1 and pn
+     * @param p1 point p1
+     * @param pn point pn
      */
     public void findLowerHull(ArrayList<Point> pointRightOfLine, Point p1, Point pn) {
         if(!pointRightOfLine.isEmpty()){
@@ -166,18 +192,35 @@ public class ConvexHull {
                     MaxDistance = pointDistanceFromLine;
                     index = i;
                 }
+                /**
+                 * If two point have the same distance from the line, calculate the 
+                 * the angle at each point and select the point with the larger
+                 * angle as point in the convex set.
+                 */
+                else if((pointDistanceFromLine == MaxDistance) && (index != -1)){
+                    Point pointAtIndex = pointRightOfLine.get(index);
+                    Point pointAtI = pointRightOfLine.get(i);
+                    
+                    double angleAtpointAtIndexToP1andPn = findAngleOfPointFromLine(pointAtIndex,p1,pn);
+                    double angleAtpointAtIToP1andPn = findAngleOfPointFromLine(pointAtI,p1,pn);
+                    
+                    if(angleAtpointAtIToP1andPn > angleAtpointAtIndexToP1andPn){
+                        MaxDistance = pointDistanceFromLine;
+                        index = i;
+                    }
+                }
             }
             Point pMin = pointRightOfLine.get(index);
             this.convexSet.add(pMin);
             pointRightOfLine.remove(index);
-            
+
             //Find point on the left Line Pmax and P1
-            ArrayList<Point> pointOnRightOfPminPn = findPointRightOfLine(pMin,pn,pointRightOfLine);
             ArrayList<Point> pointOnRightOfP1Pmin = findPointRightOfLine(p1,pMin,pointRightOfLine);
-            
+            ArrayList<Point> pointOnRightOfPminPn = findPointRightOfLine(pMin,pn,pointRightOfLine);
+
             //Recursively find upper hull points
-            findLowerHull(pointOnRightOfPminPn, pMin, pn);
             findLowerHull(pointOnRightOfP1Pmin, p1, pMin);
+            findLowerHull(pointOnRightOfPminPn, pMin, pn);
         }
     }
     
@@ -196,6 +239,26 @@ public class ConvexHull {
         return vectorDistance/distanceBtnP1andPn;
     }
     
+    /**
+     * Find the angle at point a in a triangle with point a,p1 and pn as vertices.
+     * It uses the cosine rule to find the angle
+     * @param a point
+     * @param p1 point
+     * @param pn point
+     * @return angle at point a in degrees
+     */
+    public double findAngleOfPointFromLine(Point a, Point p1, Point pn){
+        
+        double distanceBtnP1AndPn = Math.sqrt(((p1.x - pn.x)*(p1.x - pn.x))+((p1.y - pn.y)*(p1.y - pn.y)));
+        double distanceBtnP1Anda = Math.sqrt(((p1.x - a.x)*(p1.x - a.x))+((p1.y - a.y)*(p1.y - a.y)));
+        double distanceBtnAAndPn = Math.sqrt(((a.x - pn.x)*(a.x - pn.x))+((a.y - pn.y)*(a.y - pn.y)));
+        
+        double numerator = Math.pow(distanceBtnP1Anda, 2)+Math.pow(distanceBtnAAndPn, 2)-(Math.pow(distanceBtnP1AndPn, 2));
+        double cosOfAntgleAtPointA = numerator/(2*distanceBtnP1Anda*distanceBtnP1AndPn);
+        
+        return Math.toDegrees(Math.acos(cosOfAntgleAtPointA));
+        
+    }
     
     /**
      * Copy the the point from an array list to a one dimensional array
@@ -211,6 +274,16 @@ public class ConvexHull {
 
         return pointsArray;
     }
+    
+    public static void printArray(ArrayList<Point> points){
+        
+        for( int i = 0; i < points.size(); i ++){
+            System.out.print(points.get(i) + ":");
+        }
+        System.out.println();
+    }
+    
+    
     /**
      * @param args the command line arguments
      */
@@ -239,7 +312,9 @@ public class ConvexHull {
             Point [] points = ConvexHull.getPointArray(dataPoint);
             ConvexHull convexHullObject = new ConvexHull();
             //System.out.println(dataPoint.size());
-            System.out.println(convexHullObject.findConvexHull(points).toString());
+            HashSet<Point> convexSet = convexHullObject.findConvexHull(points);
+            System.out.println(convexSet.size());
+            System.out.println(convexSet.toString());
 
         } catch (FileNotFoundException e) {
             System.err.println("Eror : File Not Found" + e.toString());
